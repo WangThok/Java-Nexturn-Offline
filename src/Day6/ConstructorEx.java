60bdd5b99cdebe1b03f7202748021f05c8c89702
Wangamba <thokgeorge@gmail.com>
2025-03-06 11:43:03 +0530

    Resolved merge conflicts

--- src/Day6/ConstructorEx.java
+++ src/Day6/ConstructorEx.java
@@ -2,7 +2,7 @@ public class ConstructorEx {
     int num=345;
     string name ="Wang";
     public static void main(String[] args) {
-        Constructor con=
-        System.out.println("Hello, World!");
+        ConstructorEx obj = new ConstructorEx();
+        System.out.println(obj.name);
     }
 }
