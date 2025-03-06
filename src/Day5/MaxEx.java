fcf9986d8b79e64ef2f2b8dc736e08f3af18bf71
Wangamba <thokgeorge@gmail.com>
2025-03-06 10:41:01 +0530

    Updated file with latest changessssss

---
+++ src/Day5/MaxEx.java
@@ -0,0 +1,5 @@
+public class MaxEx {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/Day6/ConstructorEx.java
@@ -0,0 +1,8 @@
+public class ConstructorEx {
+    int num=345;
+    string name ="Wang";
+    public static void main(String[] args) {
+        Constructor con=
+        System.out.println("Hello, World!");
+    }
+}
